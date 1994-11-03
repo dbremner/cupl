fedc892f81e9f91e9901a016f21e6cda509b819a
Eric S. Raymond <esr@thyrsus.com>
1994-11-03 15:14:16 +0000

    Initial revision

---
+++ execute.c
@@ -0,0 +1,21 @@
+/*****************************************************************************
+
+NAME
+   execute.c -- parse-tree execution
+
+SYNOPSIS
+   void execute(node *tree)	-- execute a parse tree
+
+DESCRIPTION 
+   This code does execution of a CUPL parse tree.  Actual execution is
+handed off to execute().
+
+*****************************************************************************/
+/*LINTLIBRARY*/
+#include "cupl.h"
+
+void execute(node *tree)
+{
+}
+
+/* execute.c ends here */
